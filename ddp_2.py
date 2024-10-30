1042bccc0ae25a0c6aa7098a0df5f2924d88692a
ayangnyaluffy <anggiadwihikmah19@gmail.com>
2024-10-30 16:03:03 +0700

    pertemuan 6

--- ddp_2.py
+++ ddp_2.py
@@ -1,25 +1,4 @@
-pilihan = int(input("""pilih nomor pilihan :  
-                1. menghitung luas persegi 
-                2. menghitung luas lingkaran 
-                3. menghitung luas segitiga"""))
-match pilihan:
-    case 1:
-        print("menghitung luas persegi")
-        s = int(input("masukkan nilai sisi: "))
-        luaspersegi = s * s
-        print(f"luas persegi dengan sisi {s} adalah {luaspersegi}")
-    case 2:
-        print("menghitung luas lingkaran")
-        pi = 3.14
-        r = int(input("masukkan nilai jari-jari"))
-        luaslingkaran = pi * r** 2
-        print(f"luas lingkaran dengan jari jari {r} adalah {luaslingkaran}") 
-    case 3:
-        print("menghitung luas segitiga")
-        alas = int(input("masukkan nilai alas: "))
-        tinggi = int(input("masukkan nilai tinggi: "))
-        luasSegitiga = 1/2 * alas * tinggi
-        print(f"luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luasSegitiga}")
-    case _:
-        print("input tidak valid")
-        
+hasil = 0
+for i in range(1,20, 2):
+    hasil += i
+print(f" 1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = {hasil}")
